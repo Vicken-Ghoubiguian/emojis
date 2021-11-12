3d46b3dd49312c03840e75c563900615d9ab7537
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-11-12 20:52:53 +0100

    All getters

--- emojis.go
+++ emojis.go
@@ -20,3 +20,39 @@ func (ej *Emoji) InitializeWeatherModule(slug string, character string, unicodeN
 	ej.group = group
 	ej.subGroup = subGroup
 }
+
+//
+func (ej *Emoji) getSlug() string {
+
+	return ej.slug
+}
+
+//
+func (ej *Emoji) getCharacter() string {
+
+	return ej.character
+}
+
+//
+func (ej *Emoji) getUnicodeName() string {
+
+	return ej.unicodeName
+}
+
+//
+func (ej *Emoji) getCodePoint() string {
+
+	return ej.codePoint
+}
+
+//
+func (ej *Emoji) getGroup() string {
+
+	return ej.group
+}
+
+//
+func (ej *Emoji) getSubGroup() string {
+
+	return ej.subGroup
+}
