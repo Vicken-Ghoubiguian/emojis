8f8c87ac8a45ef6d1e0ede00c6b275745cbbac12
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-11-12 20:47:29 +0100

    HJ

--- emojis.go
+++ emojis.go
@@ -9,3 +9,14 @@ type Emoji struct {
 	group       string
 	subGroup    string
 }
+
+// Defining the Emoji initializer
+func (ej *Emoji) InitializeWeatherModule(slug string, character string, unicodeName string, codePoint string, group string, subGroup string) {
+
+	ej.slug = slug
+	ej.character = character
+	ej.unicodeName = unicodeName
+	ej.codePoint = codePoint
+	ej.group = group
+	ej.subGroup = subGroup
+}
