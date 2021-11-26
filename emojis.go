5177ebba891718042c68498048a1aeeebc788026
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-11-26 12:07:03 +0100

    For cat 5

--- emojis.go
+++ emojis.go
@@ -209,13 +209,13 @@ func (catg *Category) GetSubCategoriesAccount() int {
  * 'LISTOFCATEGORIES' STRUCTURE TO DEFINE A CATEGORY'S MAP WITH ALL RELATED DATAS CONTAINING ALL EMOJIS CATEGORIES WITH ALL OF THEIR DATAS FROM THE OPEN EMOJI API...
  */
 
-//
+// Defining the type 'ListOfCategories' which define a list of emojis categories...
 type ListOfCategories struct {
 	mapOfCategories   map[string]Category
 	categoriesAccount int
 }
 
-//
+// Defining the ListOfCategories initializer...
 func (loc *ListOfCategories) InitializeListOfCategories(listOfCategories map[string]Category) {
 
 	loc.mapOfCategories = listOfCategories
