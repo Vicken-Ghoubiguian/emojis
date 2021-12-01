1b33a570e704062b10356a3864ece37de2b669f5
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-12-01 09:23:44 +0100

    Initializer for error

--- emojisError.go
+++ emojisError.go
@@ -2,6 +2,15 @@ package emojis
 
 //
 type EmojisError struct {
+	code    int
+	status  string
+	message string
+}
 
+//
+func (ee *EmojisError) InitializeEmojisError(code int, status string, message string) {
 
+	ee.code = code
+	ee.status = status
+	ee.message = message
 }
