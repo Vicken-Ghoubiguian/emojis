ad7686b32655f0320f870d96290b8db37fc4c3e7
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-02 19:44:46 +0100

    gtyu

--- setTheory.go
+++ setTheory.go
@@ -9,16 +9,16 @@ func (loe *ListOfEmojis) Intersection(currentListOfEmojis ListOfEmojis) ListOfEm
 	//
 	var intersectionEmojis ListOfEmojis
 
-	//
+	// Definition of the 'intersectionEmojisMap' map[string]Emoji which will contain all Emojis from the intersection of the 'loe' ListOfEmojis and the 'currentListOfEmojis' ListOfEmojis...
 	intersectionEmojisMap = make(map[string]Emoji)
 
-	//
+	// Initialization of the main loop of this function...
 	for _, emoji := range loe.GetListOfEmojis() {
 
-		//
+		// If the 'currentListOfEmojis' ListOfEmojis does not contains the 'emoji' Emoji...
 		if currentListOfEmojis.Contains(emoji) {
 
-			//
+			// Put the emoji inside the 'intersectionEmojisMap' map[string]Emoji...
 			intersectionEmojisMap[emoji.GetSlug()] = emoji
 		}
 	}
@@ -42,25 +42,25 @@ func (loe *ListOfEmojis) Union(currentListOfEmojis ListOfEmojis) ListOfEmojis {
 	//
 	unionEmojisMap = make(map[string]Emoji)
 
-	//
+	// Initialization of the first main loop of this function...
 	for _, emoji := range loe.GetListOfEmojis() {
 
-		//
+		// Put the emoji inside the 'unionEmojisMap' map[string]Emoji...
 		unionEmojisMap[emoji.GetSlug()] = emoji
 	}
 
-	//
+	// Initialization of the second main loop of this function...
 	for _, emoji := range currentListOfEmojis.GetListOfEmojis() {
 
-		//
+		// If the 'loe' ListOfEmojis does not contains the 'emoji' Emoji...
 		if !loe.Contains(emoji) {
 
-			//
+			// Put the emoji inside the 'unionEmojisMap' map[string]Emoji...
 			unionEmojisMap[emoji.GetSlug()] = emoji
 		}
 	}
 
-	//
+	// Definition of the 'unionEmojis' map[string]Emoji which will contain all Emojis from the union of the 'loe' ListOfEmojis and the 'currentListOfEmojis' ListOfEmojis...
 	unionEmojis.InitializeListOfEmojis(unionEmojisMap)
 
 	//
