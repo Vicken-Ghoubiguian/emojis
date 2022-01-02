88d2f9e91bedc097f91697891260a7b675703a18
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-02 17:01:02 +0100

    Summyoztza

--- setTheory.go
+++ setTheory.go
@@ -1,3 +1,27 @@
 package emojis
 
+//
+/*func (loe *ListOfEmojis) Intersection() ListOfEmojis {
+
+	return true
+}
+
+//
+func (loe *ListOfEmojis) Union() ListOfEmojis {
+
+	return true
+}
+
+//
+func (loe *ListOfEmojis) Complement() ListOfEmojis {
+
+	return true
+}
+
+//
+func (loe *ListOfEmojis) CartesianProduct() ListOfEmojis {
+
+	return true
+}*/
+
 // => YOU MUST DEFINE AND DEVELOP SOME FUNCTIONS TO MAKE OPERATIONS OF SET THEORY IN LIST OF EMOJIS...
